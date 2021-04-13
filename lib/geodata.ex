f06fd5f37daf5f67d4febe2a169d1cbb85f78735
Nuno Freire <greven.pt@gmail.com>
2021-04-13 21:58:33 +0100

    Init

---
+++ lib/geodata.ex
@@ -0,0 +1,18 @@
+defmodule Geodata do
+  @moduledoc """
+  Documentation for `Geodata`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Geodata.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/geodata_test.exs
@@ -0,0 +1,8 @@
+defmodule GeodataTest do
+  use ExUnit.Case
+  doctest Geodata
+
+  test "greets the world" do
+    assert Geodata.hello() == :world
+  end
+end
